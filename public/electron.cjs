90f35f672df7d591009814a69713cdb7448e4d14
Monuyadav1995 <monu.bigdoorsolutions@gmail.com>
2024-10-23 12:37:05 +0530

    frist commit

---
+++ public/electron.cjs
@@ -0,0 +1,31 @@
+const { app, BrowserWindow,Menu } = require('electron');
+const path = require('path');
+
+
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      icon: path.join(__dirname, 'assate', 'apple-touch-icon.png'), 
+      nodeIntegration: true
+    }
+  });
+
+   // Remove the default menu
+   Menu.setApplicationMenu(null);
+  
+
+  mainWindow.loadURL('http://localhost:5173');
+}
+
+
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
