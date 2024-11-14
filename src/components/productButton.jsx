9125611428817cd3f5690ca6909545692b689aef
Yogesh Mayekar <yogesh.mayekar09@gmail.com>
2024-11-14 23:46:41 +0530

    intigrated post api to create the new category

--- src/components/productButton.jsx
+++ src/components/productButton.jsx
@@ -1,16 +1,49 @@
 import React, { useState } from 'react';
+import axios from "axios";
+import SuccessAlert from './SuccessAlert';
+import ErrorAlert from './ErrorAlert';
+import LoadingCircle from './LoadingCircle';
 
 const ProductList = () => {
     const [isOpen, setIsOpen] = useState(false);
+    const [categoryName, setCategoryName] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+    const [tosucessMessage, settosucessMessage] = useState(null);
+    const [toErrorMessage, setErrorMessage] = useState(null);
 
     const toggleModal = () => {
         setIsOpen(!isOpen);
     };
 
+    const handleAddCategoryAPI=async(e)=>{
+        e.preventDefault();
+        setIsLoading(true);
+        const payload = { category_name: categoryName };
+        try{
+        const response = await axios.post("http://localhost:8080/api/v1/categories", payload);
+        if (response.status === 201) {
+            settosucessMessage("Category added successfully!");
+            setTimeout(() => settosucessMessage(null), 3000);
+            setIsLoading(false)
+            if (process.env.NODE_ENV === "development") {
+                console.log("Category added:", response.data);
+            }
+        }
+        }catch(error){
+            if (process.env.NODE_ENV === "development") {
+                console.error("Error adding category:", error.message);
+            }
+            setErrorMessage("Failed to add category. Please try again.");
+            setTimeout(() => setErrorMessage(null), 3000);
+            setIsLoading(false);
+        }
+    }
+
     return (
         <>
             {/* Button to open the modal */}
-            
+            {tosucessMessage && <SuccessAlert message={tosucessMessage} />}
+            {toErrorMessage && <ErrorAlert message={toErrorMessage} />}
             <button
                 onClick={toggleModal}
                 className="px-4 py-2 m-6 text-white bg-[#10B981] rounded"
@@ -21,7 +54,8 @@ const ProductList = () => {
             {/* Modal */}
             {isOpen && (
                 <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 ">
-                    <div className="relative bg-white shadow-lg rounded-xl w-96">
+                    {isLoading ? <LoadingCircle/> :
+                     <div className="relative bg-white shadow-lg rounded-xl w-96">
                         <h2 className="text-lg font-bold text-center text-white bg-[#10B981] py-3 rounded-t-xl">
                         Add Category
                         </h2>
@@ -35,6 +69,9 @@ const ProductList = () => {
                                 <input
                                     type="text"
                                     className="w-2/3 px-2 py-1 border rounded"
+                                    id="categoryName"
+                                    value={categoryName}
+                                    onChange={(e) => setCategoryName(e.target.value)}
                                     placeholder="Enter Product Name"
                                 />
                             </div>
@@ -42,11 +79,12 @@ const ProductList = () => {
 
                             {/* Submit Button */}
                             <div className="flex justify-center">
-                                <button
+                            <button
                                     type="submit"
                                     className="bg-[#10B981]  text-white px-4 py-2 rounded"
+                                    onClick={handleAddCategoryAPI}
                                 >
-                                    Add Category
+                                 Add Category
                                 </button>
                             </div>
                         </form>
@@ -58,7 +96,7 @@ const ProductList = () => {
                         >
                             &times;
                         </button>
-                    </div>
+                    </div>}
                 </div>
             )}
         </>
