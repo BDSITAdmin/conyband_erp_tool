f5d0c7b0917c2df36266b9d04e24bf6a6cd236b6
Yogesh Mayekar <yogesh.mayekar09@gmail.com>
2024-11-14 23:45:58 +0530

    created the sucess alert component

---
+++ src/components/SuccessAlert.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Alert from '@mui/material/Alert';
+
+function SuccessAlert({message}) {
+  return (
+    <>
+        {message && <Alert severity="success" className='z-50 fixed right-3 bottom-3'>{message}</Alert>}
+    </>
+  )
+}
+
+export default SuccessAlert
