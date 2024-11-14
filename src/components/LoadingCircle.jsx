244467e1c1080b49796e88742f04a56215642be3
Yogesh Mayekar <yogesh.mayekar09@gmail.com>
2024-11-14 23:45:17 +0530

    created the loading component

---
+++ src/components/LoadingCircle.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CircularProgress from '@mui/material/CircularProgress';
+
+function LoadingCircle() {
+  return (
+    <>
+        <CircularProgress color="success" />
+    </>
+  )
+}
+
+export default LoadingCircle
